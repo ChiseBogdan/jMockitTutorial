2a3e7aa3da6b99c8e79bac28702a61e309d4306c
chise_b <chise_b@yahoo.com>
2019-05-20 23:11:20 +0300

    init final

---
+++ jMockitTest/src/test/java/controller/TransactionControllerTest.java
@@ -0,0 +1,18 @@
+package controller;
+
+import domain.Person;
+import mockit.Expectations;
+import mockit.Mocked;
+import mockit.integration.junit4.JMockit;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import repository.AccountRepository;
+import repository.PersonRepository;
+
+import static org.junit.Assert.*;
+
+@RunWith(JMockit.class)
+public class TransactionControllerTest {
+
+
+}
